f4ed94bba4360415428611dd89f8b1addc350da1
Angel Mavare <angelmavare1@gmail.com>
2022-05-17 20:45:35 -0400

    images

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-
-<?php require('partials/header.php');?> 
+<?php require('partials/header.php'); ?>
 <div class="container-fluid">
     <!-- box-intro -->
     <section class="box-intro">
@@ -20,176 +19,182 @@
     </section>
     <!-- end box-intro -->
 </div>
-    <!-- portfolio div -->
-    <div class="portfolio-div">
-        <div class="portfolio">
-            <div class="no-padding portfolio_container">
-                <!-- single work -->
-                <div class="col-md-3 col-sm-6  fashion logo">
-                    <a href="single-project.html" class="portfolio_item">
-                        <img src="assets/img/portfolio/nebula-theme/ipad-nebula8.jpg" alt="image" class="img-responsive" />
-                        <div class="portfolio_item_hover">
-                            <div class="portfolio-border clearfix">
-                                <div class="item_info">
-                                    <span>Nebula theme</span>
-                                    <em>Theme</em>
-                                </div>
+<!-- portfolio div -->
+<div class="portfolio-div">
+    <div class="portfolio">
+        <div class="no-padding portfolio_container">
+            <!-- single work -->
+            <div class="col-md-3 col-sm-6  fashion logo">
+                <a href="single-project.html" class="portfolio_item">
+                    <img src="assets/img/portfolio/nebula-theme/ipad-nebula8.jpg" alt="image" class="img-responsive" />
+                    <div class="portfolio_item_hover">
+                        <div class="portfolio-border clearfix">
+                            <div class="item_info">
+                                <span>Nebula theme</span>
+                                <em>Theme</em>
                             </div>
                         </div>
-                    </a>
-                </div>
-                <!-- end single work -->
-
-                <!-- single work -->
-                <div class="col-md-3 col-sm-6 ads graphics">
-                    <a href="single-project.html" class="portfolio_item">
-                        <img src="assets/img/portfolio/coecmoodle/coecmoodle2.jpg" alt="image" class="img-responsive" />
-                        <div class="portfolio_item_hover">
-                            <div class="portfolio-border clearfix">
-                                <div class="item_info">
-                                    <span>COEC moodle</span>
-                                    <em>Moodle</em>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <!-- end single work -->
-
-                <!-- single work -->
-                <div class="col-md-6 col-sm-12 photography">
-                    <a href="single-project.html" class="portfolio_item">
-                        <img src="assets/img/portfolio/scribana/scribana-presentation2-square2.jpg" alt="image" class="img-responsive" />
-                        <div class="portfolio_item_hover">
-                            <div class="portfolio-border clearfix">
-                                <div class="item_info">
-                                    <span>Scribana.shop</span>
-                                    <em>E-commerce</em>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <!-- end single work -->
-
-                <!-- single work -->
-                <div class="col-md-3 col-sm-6 graphics ads">
-                    <a href="single-project.html" class="portfolio_item">
-                        <img src="assets/img/portfolio/vjob/vjobmockup.jpg" alt="image" class="img-responsive" />
-                        <div class="portfolio_item_hover">
-                            <div class="portfolio-border clearfix">
-                                <div class="item_info">
-                                    <span>Valuejob</span>
-                                    <em>Wordpress</em>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <!-- end single work -->
-
-                <!-- single work -->
-                <div class="col-md-3 col-sm-6 fashion ads">
-                    <a href="single-project.html" class="portfolio_item">
-                        <img src="assets/img/portfolio/04.jpg" alt="image" class="img-responsive" />
-                        <div class="portfolio_item_hover">
-                            <div class="portfolio-border clearfix">
-                                <div class="item_info">
-                                    <span>Agenda Online</span>
-                                    <em>Wordpress plugin</em>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <!-- end single work -->
-
-                
-
-                <!-- single work -->
-                <div class="col-md-6 col-sm-12 photography">
-                    <a href="single-project.html" class="portfolio_item">
-                        <img src="assets/img/portfolio/010.jpg" alt="image" class="img-responsive" />
-                        <div class="portfolio_item_hover">
-                            <div class="portfolio-border clearfix">
-                                <div class="item_info">
-                                    <span>Photorealistic smartwatch</span>
-                                    <em>Photography</em>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <!-- end single work -->
-
-                <!-- single work -->
-                <div class="col-md-3 col-sm-6 graphics ads">
-                    <a href="single-project.html" class="portfolio_item">
-                        <img src="assets/img/portfolio/06.jpg" alt="image" class="img-responsive" />
-                        <div class="portfolio_item_hover">
-                            <div class="portfolio-border clearfix">
-                                <div class="item_info">
-                                    <span>Mobile devices</span>
-                                    <em>Graphics / Ads</em>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <!-- end single work -->
-
-                <!-- single work -->
-                <div class="col-md-3 col-sm-6 graphics ads">
-                    <a href="single-project.html" class="portfolio_item">
-                        <img src="assets/img/portfolio/07.jpg" alt="image" class="img-responsive" />
-                        <div class="portfolio_item_hover">
-                            <div class="portfolio-border clearfix">
-                                <div class="item_info">
-                                    <span>Mobile devices</span>
-                                    <em>Graphics / Ads</em>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <!-- end single work -->
-
-                <!-- single work -->
-                <div class="col-md-3 col-sm-6 graphics ads">
-                    <a href="single-project.html" class="portfolio_item">
-                        <img src="assets/img/portfolio/08.jpg" alt="image" class="img-responsive" />
-                        <div class="portfolio_item_hover">
-                            <div class="portfolio-border clearfix">
-                                <div class="item_info">
-                                    <span>Mobile devices</span>
-                                    <em>Graphics / Ads</em>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <!-- end single work -->
-
-                <!-- single work -->
-                <div class="col-md-3 col-sm-6 graphics ads">
-                    <a href="single-project.html" class="portfolio_item">
-                        <img src="assets/img/portfolio/09.jpg" alt="image" class="img-responsive" />
-                        <div class="portfolio_item_hover">
-                            <div class="portfolio-border clearfix">
-                                <div class="item_info">
-                                    <span>Mobile devices</span>
-                                    <em>Graphics / Ads</em>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <!-- end single work -->
+                    </div>
+                </a>
             </div>
-            <!-- end portfolio_container -->
-        </div>
-        <!-- portfolio -->
-    </div>
-    <!-- end portfolio div -->
+            <!-- end single work -->
 
-    <?php require('partials/footer.php');?> 
+            <!-- single work -->
+            <div class="col-md-3 col-sm-6 graphics ads">
+                <a href="single-project.html" class="portfolio_item">
+                    <img src="assets/img/portfolio/cdmarin/cdmarinmockup.jpg" alt="image" class="img-responsive" />
+                    <div class="portfolio_item_hover">
+                        <div class="portfolio-border clearfix">
+                            <div class="item_info">
+                                <span>CD Marín</span>
+                                <em>Website</em>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <!-- end single work -->
+
+
+
+            <!-- single work -->
+            <div class="col-md-6 col-sm-12 photography">
+                <a href="single-project.html" class="portfolio_item">
+                    <img src="assets/img/portfolio/scribana/scribana-presentation2-square2.jpg" alt="image" class="img-responsive" />
+                    <div class="portfolio_item_hover">
+                        <div class="portfolio-border clearfix">
+                            <div class="item_info">
+                                <span>Scribana.shop</span>
+                                <em>E-commerce</em>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <!-- end single work -->
+
+            <!-- single work -->
+            <div class="col-md-3 col-sm-6 graphics ads">
+                <a href="single-project.html" class="portfolio_item">
+                    <img src="assets/img/portfolio/octoedroshop/octomockup2.jpg" alt="image" class="img-responsive" />
+                    <div class="portfolio_item_hover">
+                        <div class="portfolio-border clearfix">
+                            <div class="item_info">
+                                <span>Octoedro shop</span>
+                                <em>E-commerce</em>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <!-- end single work -->
+
+
+
+            <!-- single work -->
+            <div class="col-md-3 col-sm-6 fashion ads">
+                <a href="single-project.html" class="portfolio_item">
+                    <img src="assets/img/portfolio/uxtify_theme/uxtify-cover2.jpg" alt="image" class="img-responsive" />
+                    <div class="portfolio_item_hover">
+                        <div class="portfolio-border clearfix">
+                            <div class="item_info">
+                                <span>Uxtify theme</span>
+                                <em>Theme</em>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <!-- end single work -->
+
+
+
+            <!-- single work -->
+            <div class="col-md-6 col-sm-12 photography">
+                <a href="single-project.html" class="portfolio_item">
+                    <img src="assets/img/portfolio/pixonauta2017/pixoweb2-2.jpg" alt="image" class="img-responsive" />
+                    <div class="portfolio_item_hover">
+                        <div class="portfolio-border clearfix">
+                            <div class="item_info">
+                                <span>Photorealistic smartwatch</span>
+                                <em>Photography</em>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <!-- end single work -->
+
+            <!-- single work -->
+            <div class="col-md-3 col-sm-6 graphics ads">
+                <a href="single-project.html" class="portfolio_item">
+                    <img src="assets/img/portfolio/sshop/sshop-cover.jpg" alt="image" class="img-responsive" />
+                    <div class="portfolio_item_hover">
+                        <div class="portfolio-border clearfix">
+                            <div class="item_info">
+                                <span>SMS Shop</span>
+                                <em>E-commerce</em>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <!-- end single work -->
+
+
+
+            <!-- single work -->
+            <div class="col-md-3 col-sm-6 graphics ads">
+                <a href="single-project.html" class="portfolio_item">
+                    <img src="assets/img/portfolio/vjob/vjobmockup.jpg" alt="image" class="img-responsive" />
+                    <div class="portfolio_item_hover">
+                        <div class="portfolio-border clearfix">
+                            <div class="item_info">
+                                <span>Valuejob</span>
+                                <em>Website</em>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <!-- end single work -->
+
+            <!-- single work -->
+            <div class="col-md-3 col-sm-6 ads graphics">
+                <a href="single-project.html" class="portfolio_item">
+                    <img src="assets/img/portfolio/coecmoodle/coecmoodle2.jpg" alt="image" class="img-responsive" />
+                    <div class="portfolio_item_hover">
+                        <div class="portfolio-border clearfix">
+                            <div class="item_info">
+                                <span>COEC moodle</span>
+                                <em>Moodle</em>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <!-- end single work -->
+
+            <!-- single work -->
+            <div class="col-md-3 col-sm-6 graphics ads">
+                <a href="single-project.html" class="portfolio_item">
+                    <img src="assets/img/portfolio/09.jpg" alt="image" class="img-responsive" />
+                    <div class="portfolio_item_hover">
+                        <div class="portfolio-border clearfix">
+                            <div class="item_info">
+                                <span>Mobile devices</span>
+                                <em>Graphics / Ads</em>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <!-- end single work -->
+        </div>
+        <!-- end portfolio_container -->
+    </div>
+    <!-- portfolio -->
+</div>
+<!-- end portfolio div -->
+
+<?php require('partials/footer.php'); ?>
